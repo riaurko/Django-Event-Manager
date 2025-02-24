e297add1ddb05105aba0515efa15c21a6b321537
riaurko <riaurko@gmail.com>
2025-02-24 22:57:13 +0600

    Fixing 1

--- users/signals.py
+++ users/signals.py
@@ -29,8 +29,6 @@ def send_activation_email(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def assign_role(sender, instance, created, **kwargs):
     if created:
-        user_group, created = Group.objects.get_or_create(name='User')
         participant_group = Group.objects.get(name='Participant')
-        instance.groups.add(user_group)
         instance.groups.add(participant_group)
         instance.save()
